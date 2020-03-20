7bc1e396b8d60d040ac0ba2a98bb3fc3960ac9f0
Krishnajirao <404341@cognizant.com>
2020-03-20 19:26:39 +0530

    assignment 5

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -0,0 +1,19 @@
+package io.pivotal.pal.tracker;
+
+import java.time.LocalDate;
+
+import java.util.List;
+
+public interface TimeEntryRepository {
+
+    TimeEntry create(TimeEntry timeEntry);
+
+    TimeEntry find(Long id);
+
+    List<TimeEntry> list();
+
+    TimeEntry update(Long id,TimeEntry timeEntry);
+
+    void delete(Long id);
+
+}
