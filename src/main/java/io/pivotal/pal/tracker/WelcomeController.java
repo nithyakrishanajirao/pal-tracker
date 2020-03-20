c7e083fe2ce20365786adf0b2ac7735e780ed23d
Krishnajirao <404341@cognizant.com>
2020-03-20 16:59:15 +0530

    second assement

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,19 +1,35 @@
 package io.pivotal.pal.tracker;
 
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
+
 public class WelcomeController {
-String message;
+
+    private final String message;
+
+
+
     @GetMapping("/")
-    public String sayHello() {
-        return "hello";
+
+    public String sayHello() {        //return "hello";
+
+        return message;
+
     }
+
+
+
     public WelcomeController(@Value("${welcome.message}") String message) {
 
         this.message = message;
 
     }
+
 }
